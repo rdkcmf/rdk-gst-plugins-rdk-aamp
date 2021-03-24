ab4afa5ae1a7c6a4230fc3ceb099ea985da2e504
Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
2021-03-24 18:13:49 +0000

    DELIA-48964: SYST_ERROR_JSObjNull marker is increased on 4.8p1
    Reason for change: Add stub code for newly added JSObjectDeleteProperty
    Test Procedure: Check for regressions
    Risks: Low
    
    
    Signed-off-by: Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
    Change-Id: I4d024f005196099ac0c72bccffbcd3b6e522eaf7

--- drm/ave/StubsForAVEPlayer.cpp
+++ drm/ave/StubsForAVEPlayer.cpp
@@ -77,6 +77,7 @@ void JSValueProtect(){ }
 void JSStringCreateWithCharacters(){ }
 void JSObjectMakeDate(){ }
 void JSObjectMakeError(){ }
+void JSObjectDeleteProperty(){ }
 }
 #endif
 #ifndef REALTEKCE
