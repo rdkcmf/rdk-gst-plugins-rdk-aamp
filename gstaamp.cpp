b1338e4d85c08a1106acfd260d059d595c74e775
Jaikumar Subramanian <jai_kumar@cable.comcast.com>
2022-12-06 20:33:19 +0000

    Revert "DELIA-58262: TTML subs not displayed on Foxtel"
    This reverts commit 66e0038d3bb69fd4cc2ea3b77e5400971231f6f6.
    
    Reason for revert: Causing DELIA-58262
    
    Change-Id: I2d7d8ce6110d1571509872bf15f7a3857eb20e4b

--- gstaamp.cpp
+++ gstaamp.cpp
@@ -571,7 +571,7 @@ public:
 	 * @param[in] initFragment flag to indicate init header
 	 * @note Ownership of pBuffer is transferred
 	 */
-	void SendTransfer(MediaType mediaType, GrowableBuffer* pBuffer, double fpts, double fdts, double fDuration, bool initFragment = false, bool discontinuity = false)
+	void SendTransfer(MediaType mediaType, GrowableBuffer* pBuffer, double fpts, double fdts, double fDuration, bool initFragment = false)
 	{
 		SendHelper(mediaType, pBuffer->ptr, pBuffer->len, fpts, fdts, fDuration, false /*transfer*/);
 
